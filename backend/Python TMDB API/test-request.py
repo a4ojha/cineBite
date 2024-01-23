afe550a8bc9db84fcef7654828fd77ca5d847c22
Adon Ojha <146292475+a4ojha@users.noreply.github.com>
2024-01-22 22:15:25 -0500

    Update test-request.py

--- backend/Python TMDB API/test-request.py
+++ backend/Python TMDB API/test-request.py
@@ -3,10 +3,10 @@
 import json
 import requests
 
-key = 'c1b0a78498516cb449a9aef714e39d48'
+key = ''
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWIwYTc4NDk4NTE2Y2I0NDlhOWFlZjcxNGUzOWQ0OCIsInN1YiI6IjY1YTIxNDZjZDM1ZGVhMDEyY2Q0OWM4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5xZjVsmjV5w5pOUb_yTuerdi-PuqtUJ1nHhTIx9tKlE"
+    "Authorization": ""
 }
 
 
