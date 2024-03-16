8b843e8cb58b7a50a7f713336d56c9d6345857a4
Adon Ojha <a4ojha@uwaterloo.ca>
2024-03-15 20:42:21 -0400

    change api root URL

--- frontend/cinebite-v1/src/api/axiosConfig.js
+++ frontend/cinebite-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://cinebite-api.onrender.com',
     headers: {"skip-browser-warning": "true"}
 })
